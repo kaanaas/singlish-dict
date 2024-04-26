e4dc81aba83fce9eaa451fb3748c1ca3f0492243
kaanaas <76652290+kaanaas@users.noreply.github.com>
2024-04-26 16:49:39 +0800

    port

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(PORT);
+app.listen(port);
 
 
 /* import generated Trie */
