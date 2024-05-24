c7c89da1b45b9573ff16c7103575e4c3194de90e
kaanaas <76652290+kaanaas@users.noreply.github.com>
2024-05-25 02:54:38 +0800

    cha-

--- public/lists/lang/teochew.js
+++ public/lists/lang/teochew.js
@@ -1,1 +1,1 @@
-const langTerms = ["ah","ah balling","ah che","ah ma","ah nia","ah pui","ai pi ai chee","ai pi ai chee ai tua liap ni","already","ang ko li","ang mo kio","ang moh kui"]; module.exports = langTerms;
+const langTerms = ["ah","ah balling","ah che","ah ma","ah nia","ah pui","ai pi ai chee","ai pi ai chee ai tua liap ni","already","ang ko li","ang mo kio","ang moh kui","carrot cake","catch no ball"]; module.exports = langTerms;
