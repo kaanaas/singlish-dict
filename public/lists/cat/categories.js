368b8f315799555f45021251d49296cf88cc51d1
kaanaas <76652290+kaanaas@users.noreply.github.com>
2024-05-25 22:04:39 +0800

    unignore categories.js

---
+++ public/lists/cat/categories.js
@@ -0,0 +1,13 @@
+let cats = ["cuisine",
+    "sayings",
+    "ns",
+    "vulgar",
+    "abbreviations",
+    "nature",
+    "locations",
+    "terms of address",
+    "games",
+    "drinks"
+];
+
+module.exports = cats;
