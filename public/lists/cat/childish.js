ea547087496a071d51af578ccaad3213a35277de
kaanaas <76652290+kaanaas@users.noreply.github.com>
2024-10-17 00:10:33 +0800

    updates - 1966

--- public/lists/cat/childish.js
+++ public/lists/cat/childish.js
@@ -1,1 +1,1 @@
-const cat = ["bear bear","bird bird","friend","gai gai","gao gao","ku ku bird","ku ku jiao","ng ng","or or","orbi","orbi good","orbi quek","orh","orh hor","pat pat","pong pong","poot poot","shee shee","yucks","zhut zhut"]; module.exports = cat;
+const cat = ["bear bear","bird bird","friend","gai gai","gao gao","ku ku bird","ku ku jiao","neh nanny boo boo","neh neh","neh neh pok","ng ng","or or","orbi","orbi good","orbi quek","orh","orh hor","pat pat","pong pong","poot poot","shee shee","yucks","zhut zhut"]; module.exports = cat;
